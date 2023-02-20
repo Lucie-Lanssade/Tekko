1a2a3cd74033cb954efccac648a94a56d57a8cd6
Jean-Luc <103611121+jltp2c@users.noreply.github.com>
2023-02-20 15:55:46 +0100

    homepage init and navbar

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-require('./db')();
-const { config } = require('dotenv');
-const express = require('express');
+require("./db")();
+const { config } = require("dotenv");
+const express = require("express");
 const app = express();
-require('./config')(app);
-app.use(express.static(__dirname + '/public'));
+require("./config")(app);
+app.use(express.static(__dirname + "/public"));
+const hbs = require("hbs");
 
-const allRoutes = require('./routes/index.js');
-app.use('/api', allRoutes);
+const allRoutes = require("./routes/index.js");
+app.use("/api", allRoutes);
 
-require('./error-handling')(app);
+require("./error-handling")(app);
 module.exports = app;
