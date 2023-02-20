9310f762df41e1d2159a4a1a8bc5499f3dd90e24
Lucie-Lanssade <lucie.lanssade@gmail.com>
2023-02-20 16:59:02 +0100

    auth init2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,4 +2,19 @@ const router = require('express').Router();
 const user = require('../models/user.model');
 const mongoose = require('mongoose');
 
+/* GET signup page */
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup');
+});
+/* POST signup page */
+
+/* GET  login page */
+router.get('/login', (req, res, next) => {
+  res.render('auth/login');
+});
+// POST login page*/
+
+//POST logout
+//change commit
+
 module.exports = router;
