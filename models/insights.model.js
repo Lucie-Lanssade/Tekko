deaad17d5e85129d1d18df419477933922582c45
Lucie-Lanssade <lucie.lanssade@gmail.com>
2023-02-20 14:45:33 +0100

    models additions

--- models/insights.model.js
+++ models/insights.model.js
@@ -1,20 +1,24 @@
 const { Schema, model } = require('mongoose');
 
-const InsightsSchema = new Schema({
-  company: { type: String, required: true },
-  title: { type: String, required: true },
-  location: { type: String, required: true },
-  compensation: { type: Number, required: true },
-  level: { type: Number, enum: ['junior', 'intermediate', 'senior'] },
-  company_xp: { type: Number },
-  total_xp: { type: Number, required: true },
-  company_note: {
-    type: Number,
-    required: true,
-    enum: [0, 1, 2, 3, 4, 5, 6, 7, 6, 7, 8, 9, 10],
+const InsightsSchema = new Schema(
+  {
+    company: { type: String, required: true },
+    title: { type: String, required: true },
+    location: { type: String, required: true },
+    compensation: { type: Number, required: true },
+    level: { type: Number, enum: ['junior', 'intermediate', 'senior'] },
+    company_xp: { type: Number },
+    total_xp: { type: Number, required: true },
+    company_note: {
+      type: Number,
+      required: true,
+      enum: [0, 1, 2, 3, 4, 5, 6, 7, 6, 7, 8, 9, 10],
+    },
+    company_review: { type: String },
   },
-  company_review: { type: String },
-});
+  { timestamps: true }
+);
+
 const insights = model('Insights', InsightsSchema);
 
 module.exports = insights;
