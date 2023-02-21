bdd5e1e94e7c1ee69b4f3d4f660f0b16501e003e
Jean-Luc <103611121+jltp2c@users.noreply.github.com>
2023-02-21 22:08:54 +0100

    to pull

--- middlewares/route-guard.js
+++ middlewares/route-guard.js
@@ -13,7 +13,7 @@ const isLoggedOut = (req, res, next) => {
   }
   next();
 };
-
+//la fonction middleware exposeUserToView vérifie si l'objet de requête (request object) contient une propriété currentUser dans la session. Si c'est le cas, la fonction middleware ajoute une propriété currentUser à l'objet "res.locals" qui est utilisé pour stocker des variables locales de vue. Cette propriété currentUser est définie comme la valeur de req.session.currentUser.
 const exposeUserToView = (req, res, next) => {
   if (req.session.currentUser) {
     res.locals.currentUser = req.session.currentUser;
