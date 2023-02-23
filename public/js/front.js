35bf7196c6c9065c36a32753b6c5fa67aad0fe0a
Lucie-Lanssade <lucie.lanssade@gmail.com>
2023-02-23 18:53:06 +0100

    adding external url

--- public/js/front.js
+++ public/js/front.js
@@ -1,29 +1,30 @@
 //create
-const userContainer = document.querySelector(".userContainer");
-const companyInput = document.getElementById("company");
-const titleInput = document.getElementById("title");
-const locationInput = document.getElementById("location");
-const compensationInput = document.getElementById("compensation");
-const levelInput = document.getElementById("level");
-const companyXpInput = document.getElementById("companyXp");
-const noteInput = document.getElementById("note");
-const totalXpInput = document.getElementById("totalXp");
-const companyReviewInput = document.getElementById("companyReview");
+const userContainer = document.querySelector('.userContainer');
+const companyInput = document.getElementById('company');
+const titleInput = document.getElementById('title');
+const locationInput = document.getElementById('location');
+const compensationInput = document.getElementById('compensation');
+const levelInput = document.getElementById('level');
+const companyXpInput = document.getElementById('companyXp');
+const noteInput = document.getElementById('note');
+const totalXpInput = document.getElementById('totalXp');
+const companyReviewInput = document.getElementById('companyReview');
 //update
-const idEdit = document.getElementById("idEdit");
-const companyEdit = document.getElementById("companyEdit");
-const titleEdit = document.getElementById("titleEdit");
-const locationEdit = document.getElementById("locationEdit");
-const compensationEdit = document.getElementById("compensationEdit");
-const levelEdit = document.getElementById("levelEdit");
-const companyXpEdit = document.getElementById("companyXpEdit");
-const noteEdit = document.getElementById("noteEdit");
-const totalXpEdit = document.getElementById("totalXpEdit");
-const companyReviewEdit = document.getElementById("companyReviewEdit");
-const formCreateMsg = document.querySelector(".form-create");
-const btnDelete = document.getElementById("btnDelete");
+const idEdit = document.getElementById('idEdit');
+const companyEdit = document.getElementById('companyEdit');
+const titleEdit = document.getElementById('titleEdit');
+const locationEdit = document.getElementById('locationEdit');
+const compensationEdit = document.getElementById('compensationEdit');
+const levelEdit = document.getElementById('levelEdit');
+const companyXpEdit = document.getElementById('companyXpEdit');
+const noteEdit = document.getElementById('noteEdit');
+const totalXpEdit = document.getElementById('totalXpEdit');
+const companyReviewEdit = document.getElementById('companyReviewEdit');
+const formCreateMsg = document.querySelector('.form-create');
+const btnDelete = document.getElementById('btnDelete');
 
-const myUrl = "http://127.0.0.1:5005/insights";
+// const myUrl = "http://127.0.0.1:5005/insights";
+const myUrl = 'https://tekko.onrender.com/insights';
 
 //permet de nous renvoyer les cookies de l'utilisateur
 axios.defaults.withCredentials = true;
@@ -32,28 +33,28 @@ function displayUser(element) {
   const cloneUser = template.content.cloneNode(true);
   //recuperer le nom de la compagnie pose probleme car on recupere que l'id ... trouver le moyen d'attendre l'objet de company
 
-  cloneUser.querySelector(".userCompany span").textContent =
+  cloneUser.querySelector('.userCompany span').textContent =
     element.company.name;
-  cloneUser.querySelector(".userLocation span").textContent = element.location;
-  cloneUser.querySelector(".userJob span").textContent = element.title;
-  cloneUser.querySelector(".userCompensation span").textContent =
+  cloneUser.querySelector('.userLocation span').textContent = element.location;
+  cloneUser.querySelector('.userJob span').textContent = element.title;
+  cloneUser.querySelector('.userCompensation span').textContent =
     element.compensation;
-  cloneUser.querySelector(".userLevel span").textContent = element.level;
-  cloneUser.querySelector(".userNote span").textContent = element.company_note;
-  cloneUser.querySelector(".userXp span").textContent = element.total_xp;
-  cloneUser.querySelector(".jobXp span").textContent = element.company_xp;
-  cloneUser.querySelector(".userComment span").textContent =
+  cloneUser.querySelector('.userLevel span').textContent = element.level;
+  cloneUser.querySelector('.userNote span').textContent = element.company_note;
+  cloneUser.querySelector('.userXp span').textContent = element.total_xp;
+  cloneUser.querySelector('.jobXp span').textContent = element.company_xp;
+  cloneUser.querySelector('.userComment span').textContent =
     element.company_review;
-  cloneUser.querySelector(".UpdateBtn").href = `/insights/${element._id}/edit`;
+  cloneUser.querySelector('.UpdateBtn').href = `/insights/${element._id}/edit`;
   cloneUser.querySelector(
-    ".btnDelete"
+    '.btnDelete'
   ).href = `/insights/${element._id}/delete`;
   userContainer.append(cloneUser);
 }
 
 async function displayAll() {
   try {
-    const { data } = await axios.get(myUrl + "/userInfos");
+    const { data } = await axios.get(myUrl + '/userInfos');
     userContainer.innerHTML = null;
 
     if (data) {
@@ -63,7 +64,7 @@ async function displayAll() {
         displayUser(user);
       }
     } else {
-      userContainer.textContent = "Oups... no result...";
+      userContainer.textContent = 'Oups... no result...';
     }
   } catch (error) {
     console.log(error);
@@ -97,9 +98,9 @@ async function createUser(event) {
     const { data } = await axios.post(myUrl, userCreate);
     console.log(data);
     displayAll();
-    const msg = document.createElement("p");
-    msg.style.color = "green";
-    msg.innerHTML = "Congrats ! Your review is now created ! ";
+    const msg = document.createElement('p');
+    msg.style.color = 'green';
+    msg.innerHTML = 'Congrats ! Your review is now created ! ';
     formCreateMsg.appendChild(msg);
   } catch (error) {
     console.log(error);
@@ -132,7 +133,7 @@ async function updateReview(event) {
   };
 
   try {
-    const review = await axios.patch(myUrl + "/userInfos/" + id, reviewEdit);
+    const review = await axios.patch(myUrl + '/userInfos/' + id, reviewEdit);
     console.log(review);
     await displayAll();
   } catch (error) {
@@ -157,4 +158,4 @@ async function updateReview(event) {
 //   });
 
 displayAll();
-document.querySelector("#createForm").addEventListener("submit", createUser);
+document.querySelector('#createForm').addEventListener('submit', createUser);
